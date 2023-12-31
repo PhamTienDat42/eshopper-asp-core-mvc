812b75a9e9eb6c17a02393764f75c7b184d6ba0d
PhamTienDat42 <117574501+PhamTienDat42@users.noreply.github.com>
2024-01-01 00:14:18 +0700

    make navbar and imagebar with components

---
+++ EShopper/Components/Imagebar.cs
@@ -0,0 +1,21 @@
+﻿using EShopper.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShopper.Components
+{
+    public class Imagebar:ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Imagebar(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View("Index", _context.Categories.ToList());
+        }
+    }
+}
